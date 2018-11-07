bde5959075907e2959123b45327696fad1c6c82d
James Massey:git config --global user.name James <jammassey@hotmail.com>
2018-11-07 01:19:28 +0000

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkPlease.Models
+{
+    public class User
+    {
+        [Required]
+        [Key]
+        public int Key { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; }
+
+        [MinLength(5)]
+        [MaxLength(14)]
+        [Required]
+        [RegularExpression(@"^((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).+$")]  //Password Validation
+        public string Password { get; set; }
+    }
+
+}
